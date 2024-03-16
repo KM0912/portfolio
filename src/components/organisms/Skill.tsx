0e8d8d1fea90558bf554d234af720ae3885008c2
KM0912 <61630275+KM0912@users.noreply.github.com>
2024-03-16 19:36:54 +0900

    スキルアイコンを追加

--- src/components/organisms/Skill.tsx
+++ src/components/organisms/Skill.tsx
@@ -1,16 +1,83 @@
 import React from "react";
-import { styled, Box, Typography } from "@mui/material";
+import { Box, Typography, styled } from "@mui/material";
+import Contents from "./Contents";
 
-const StyledBox = styled(Box)({
-  padding: "16px",
+const SkillList = {
+  frontEnd: [
+    { src: "/images/skill/javascript.svg", alt: "JavaScript" },
+    { src: "/images/skill/typescript.svg", alt: "TypeScript" },
+  ],
+  backEnd: [
+    { src: "/images/skill/php.svg", alt: "php" },
+    { src: "/images/skill/laravel.svg", alt: "laravel" },
+    { src: "/images/skill/cakephp.svg", alt: "cakephp" },
+    { src: "/images/skill/react.svg", alt: "react" },
+  ],
+  database: [
+    { src: "/images/skill/mysql.svg", alt: "MySQL" },
+    { src: "/images/skill/postgresql.svg", alt: "PostgreSQL" },
+    { src: "/images/skill/sql-server.svg", alt: "SQLServer" },
+  ],
+  tool: [
+    { src: "/images/skill/git.svg", alt: "Git" },
+    { src: "/images/skill/github.svg", alt: "GitHub" },
+    { src: "/images/skill/aws.svg", alt: "AWS" },
+    { src: "/images/skill/docker.svg", alt: "Docker" },
+  ],
+  other: [
+    { src: "/images/skill/nextjs.svg", alt: "Next.js" },
+    { src: "/images/skill/express.svg", alt: "Express" },
+  ],
+};
+
+const StyledImg = styled("img")({
+  width: "150px",
+  height: "150px",
+  objectFit: "contain",
 });
 
 const Skill = () => {
-  return (
-    <StyledBox>
-      <Typography variant="h2">Skill</Typography>
-    </StyledBox>
+  const contents = (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      width="100%"
+      gap={2}
+    >
+      <Typography variant="h3">front-end</Typography>
+      <Box display="flex" justifyContent="space-around" width="100%">
+        {SkillList.frontEnd.map((skill, index) => (
+          <StyledImg key={index} src={skill.src} alt={skill.alt} />
+        ))}
+      </Box>
+      <Typography variant="h3">back-end</Typography>
+      <Box display="flex" justifyContent="space-around" width="100%">
+        {SkillList.backEnd.map((skill, index) => (
+          <StyledImg key={index} src={skill.src} alt={skill.alt} />
+        ))}
+      </Box>
+      <Typography variant="h3">database</Typography>
+      <Box display="flex" justifyContent="space-around" width="100%">
+        {SkillList.database.map((skill, index) => (
+          <StyledImg key={index} src={skill.src} alt={skill.alt} />
+        ))}
+      </Box>
+      <Typography variant="h3">tool</Typography>
+      <Box display="flex" justifyContent="space-around" width="100%">
+        {SkillList.tool.map((skill, index) => (
+          <StyledImg key={index} src={skill.src} alt={skill.alt} />
+        ))}
+      </Box>
+      <Typography variant="h3">other</Typography>
+      <Box display="flex" justifyContent="space-around" width="100%">
+        {SkillList.other.map((skill, index) => (
+          <StyledImg key={index} src={skill.src} alt={skill.alt} />
+        ))}
+      </Box>
+    </Box>
   );
+  return <Contents title="Skill" contents={contents} />;
 };
 
 export default Skill;
