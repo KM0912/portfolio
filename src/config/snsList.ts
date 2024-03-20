b2c33e5795b023d0a6bb719c398b0d035415ec4e
KM0912 <61630275+KM0912@users.noreply.github.com>
2024-03-20 22:54:55 +0900

    SNSコンポーネントにSNSリストを追加

---
+++ src/config/snsList.ts
@@ -0,0 +1,27 @@
+type SnsItem = {
+  name: string;
+  src: string;
+  alt: string;
+  link: string;
+};
+
+export const SnsList: SnsItem[] = [
+  {
+    name: "GitHub",
+    src: "images/sns/github.svg",
+    alt: "GitHub",
+    link: "https://github.com/KM0912",
+  },
+  {
+    name: "Zenn",
+    src: "images/sns/zenn.svg",
+    alt: "Zenn",
+    link: "https://zenn.dev/torakm",
+  },
+  {
+    name: "X",
+    src: "images/sns/x.svg",
+    alt: "X",
+    link: "https://twitter.com/torao_km",
+  },
+];
