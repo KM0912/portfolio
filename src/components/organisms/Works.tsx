8e45aa4bcb97e2d30fb28e63716856f8722dc6a0
KM0912 <61630275+KM0912@users.noreply.github.com>
2024-03-28 09:55:39 +0900

    WorksコンポーネントにGridコンポーネントを追加

--- src/components/organisms/Works.tsx
+++ src/components/organisms/Works.tsx
@@ -1,14 +1,36 @@
 import React from "react";
-import { styled, Box, Typography } from "@mui/material";
+import { styled, Box, Typography, Grid } from "@mui/material";
+import { workList } from "../../config/workList";
 
 const StyledBox = styled(Box)({
   padding: "16px",
+});
+
+const StyledImg = styled("img")({
+  width: "100%",
+  height: "100%",
+  objectFit: "cover",
+  borderRadius: "8px",
+  border: "1px solid #ddd",
 });
 
 const Works = () => {
   return (
     <StyledBox>
       <Typography variant="h2">Works</Typography>
+      <Grid container spacing={2}>
+        {workList.map((work, index) => (
+          <React.Fragment key={index}>
+            <Grid item xs={5}>
+              <StyledImg src={work.img} alt={work.title} />
+            </Grid>
+            <Grid item xs={7}>
+              <Typography variant="h3">{work.title}</Typography>
+              <Typography>{work.description}</Typography>
+            </Grid>
+          </React.Fragment>
+        ))}
+      </Grid>
     </StyledBox>
   );
 };
---
+++ src/config/workList.ts
@@ -0,0 +1,7 @@
+export const workList = [
+  {
+    title: "ToraKM's site",
+    description: "This is my site.",
+    img: "/images/works/tora-kms-site.png",
+  },
+];
